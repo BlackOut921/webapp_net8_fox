f1ecdedd7e177ca40ac8e41a264337f7318ac8f2
BlackOut 921 <conorhowe921@gmail.com>
2024-08-09 16:27:51 +0100

    Layout update

--- wwwroot/JavaScript.js
+++ wwwroot/JavaScript.js
@@ -1,1 +1,15 @@
 ﻿"use strict";
+
+window.onload = () => {
+    try {
+        const _observer = new IntersectionObserver(e => {
+            e.forEach(i => i.isIntersecting && i.target.classList.add("show"));
+        });
+
+        const _observerTargets = document.querySelectorAll(".observer-target");
+        _observerTargets.length > 0 && _observerTargets.forEach(i => _observer.observe(i));
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
