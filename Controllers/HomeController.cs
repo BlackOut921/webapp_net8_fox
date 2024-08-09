8b7edb1354d905686ab977451aa69cd376db74b2
BlackOut 921 <conorhowe921@gmail.com>
2024-08-09 14:36:31 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp_net8_fox.Controllers
+{
+	[AllowAnonymous]
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index() => View();
+
+		[HttpGet]
+		public IActionResult About() => View();
+
+		[HttpGet]
+		public IActionResult Contact() => View();
+	}
+}
