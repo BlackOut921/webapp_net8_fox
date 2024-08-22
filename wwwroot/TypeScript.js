e4bfd49e3b072b2d79373227cad1b09b6d7e7633
BlackOut 921 <conorhowe921@gmail.com>
2024-08-22 15:50:35 +0100

    details tag

--- wwwroot/TypeScript.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    var _observer = new IntersectionObserver(function (items) {
-        return items.forEach(function (i) { return i.isIntersecting && i.target.classList.add("show"); });
-    });
-    var _observerTargets = document.querySelectorAll(".observer-target");
-    _observerTargets.length > 0 && _observerTargets.forEach(function (i) { return _observer.observe(i); });
-};
-//# sourceMappingURL=TypeScript.js.map
--- wwwroot/TypeScript.ts
+++
@@ -1,6 +0,0 @@
-﻿window.onload = () => {
-    const _observer = new IntersectionObserver(items =>
-        items.forEach(i => i.isIntersecting && i.target.classList.add("show")));
-    const _observerTargets = document.querySelectorAll(".observer-target");
-    _observerTargets.length > 0 && _observerTargets.forEach(i => _observer.observe(i));
-};
